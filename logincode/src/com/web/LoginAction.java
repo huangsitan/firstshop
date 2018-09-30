2ac48c2664601789bc5a94589dc97e2e380fa92b
huangsitan@126.com <huangsitan@126.com>
2018-09-30 10:33:31 +0800

    ferwerw

--- logincode/src/com/web/LoginAction.java
+++ logincode/src/com/web/LoginAction.java
@@ -2,6 +2,7 @@ package com.web;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -17,38 +18,39 @@ public class LoginAction extends HttpServlet {
 
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        OutputStream out = resp.getOutputStream();
+    	resp.setContentType("text/html;charset=utf-8");
+        PrintWriter out =resp.getWriter();
 
         String username = req.getParameter("username");
 
         String password = req.getParameter("password");
 
-        String validationCode = req.getParameter("validationCode");
+        String validationCode = req.getParameter("valCode");
 
         HttpSession session = req.getSession();
 
-        String validation_code = (String)session.getAttribute("validation_code");
+        String validation_code = (String)session.getAttribute("val_code");
 
         if(validationCode.equalsIgnoreCase(validation_code)){
 
             System.out.println("验证码正确");
-            
-            
             //验证用户名密码是否正确
-
+            if(username.equals("admin")&&password.equals("123"))
+            {
+               req.getSession().setAttribute("log", username);
+               resp.sendRedirect("index.jsp");
+            
+            }else
+            {
+            	 out.print("<script>alert('用户名或密码错误');location='login.jsp';</script>");
+            }
         }else{
 
-            System.out.println("验证码错误");
+            out.print("<script>alert('验证码错误');location='login.jsp';</script>");
 
         }
 
-        
-
-        //转发到result.jsp
-
-        RequestDispatcher rd = req.getRequestDispatcher("result.jsp");
-
-        rd.forward(req, resp);
+   
 
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
